5506b7a71467ad39358ef01a3cd5fb964b6acbfa
Premkumar <108078857+g-premkumar@users.noreply.github.com>
2022-06-28 00:19:25 +0530

    Function program

---
+++ Function.py
@@ -0,0 +1,17 @@
+"""
+def addition():
+    a = 2
+    b = 10.7
+    print(a + b)
+
+#you need to call the function to print
+addition()
+"""
+def addition():
+    a = int(input("enter a number. "))
+    b = float(input("enter another number. "))
+    print(a + b)
+
+addition()
+
+#here int converts string to number
