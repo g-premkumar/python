d787b44de826a27d854d62c4ab70a0a739c88a61
g-premkumar <108078857+g-premkumar@users.noreply.github.com>
2022-06-27 23:35:20 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,35 @@
+#variables
+
+_cars = 23
+cars  = 24
+CARS = 25
+
+CARS = 253543543
+
+number_of_cars= 23
+
+kind_of_cars = "Ferrari"
+
+print(kind_of_cars)
+print(CARS)
+
+"""
+This is multi line comment
+type multiple lines
+"""
+
+name = "John jacobs"
+type_of_car = "Rolce royce"
+school = "Foundation elementary school"
+
+print(name + school)
+print(name +" "+ school)
+print(name +" works at "+ school + ".")
+
+#string formatting
+#python string.format()
+print("{} works at {}.".format(name,school))
+#format receives variables as args and sends them into curly braces in order
+
+print("{} works at {} with car {}.".format(name,school,type_of_car))
+
